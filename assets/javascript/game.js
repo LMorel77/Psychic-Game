5b155a518df20ed9d8b9b366c12d0a1d174abfe2
LMorel77 <lmorel77@aol.com>
2018-03-14 23:55:46 -0400

    Updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,10 @@ var losses = 0;
 var guesses = 10;
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("losses").innerHTML = losses;
+document.getElementById("guesses").innerHTML = guesses;
+
 document.onkeyup = function (event) {
 
     // Variable to store user input.
@@ -22,16 +26,20 @@ document.onkeyup = function (event) {
     if (userGuess == computerGuess) {
         wins++;
         guesses = 10;
+        document.getElementById("wins").innerHTML = wins;
         console.log("[Inside IF] Wins: " + wins + "; Guesses: " + guesses);
         console.log("-----------------------");
     }
     else if (guesses > 1) {
         guesses--;
+        document.getElementById("guesses").innerHTML = guesses;
         console.log("[Inside ELSE IF] Guesses remaining: " + guesses);
         console.log("-----------------------");
     } else {
         losses++;
         guesses = 10;
+        document.getElementById("losses").innerHTML = losses;
+        document.getElementById("guesses").innerHTML = guesses;
         console.log("[Inside ELSE] You lose. Resetting. Guesses: " + guesses + "; Losses: " + losses);
         console.log("-----------------------");
    }
